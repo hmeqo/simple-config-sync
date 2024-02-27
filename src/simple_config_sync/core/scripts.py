b60396aa508f174526ed9157709095d9743ba2a5
hmeqo <hmeqou@gmail.com>
2024-02-28 01:10:51 +0800

    update

--- src/simple_config_sync/core/scripts.py
+++ src/simple_config_sync/core/scripts.py
@@ -3,10 +3,7 @@ from . import config
 
 def sync():
     for op in config.options.values():
-        if op.status == 'deleted' or not op.synced:
-            op.uninstall()
-        else:
-            op.install()
+        op.sync()
     config.make_lock_file()
     config.load()
 
